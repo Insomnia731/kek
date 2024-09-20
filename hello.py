dfce4f1ee12b997f8b5bfbd84a9a6d1c952b4bc5
Insomnia731 <120348762+Insomnia731@users.noreply.github.com>
2024-09-20 19:06:21 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
+from lxml import html
+import random, requests, time#, subprocess
+
+
 print("Hello, World!")
+
+def ApiIP ():
+    while True:
+        try:
+            response = requests.post('https://api.myip.com', json={'key':'value'}, timeout=3)
+            print(response.json()['ip'])
+            break
+        except:
+            time.sleep(1)
+            print("Error")
+
+ApiIP ()
